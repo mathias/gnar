147302ab38dc6723f00a72a4fde13d8ff763d730
Matt Gauger <matt.gauger@gmail.com>
2014-03-09 15:23:00 -0500

    Talk to server side for data
    - Get list of existing links
    - Add a new link

--- src/castra/gnar/api/gnar.clj
+++ src/castra/gnar/api/gnar.clj
@@ -1,1 +1,49 @@
-(ns gnar.api.gnar)
+(ns gnar.api.gnar
+  (:refer-clojure :exclude [defn])
+  (:require [tailrecursion.castra :refer    [defn ex error *session*]]))
+
+
+(def users {"mathiasx" {:pass "password"}
+            "devin" {:pass "password"}
+            "josh" {:pass "password"}})
+
+(def links [{:href "#"
+              :title "Ruby 2.1 - Our Experience (ed note: big improvements, some incompat issues)"
+              :domain "thbpro.com"
+              :user "mathiasx"
+              :created-at "3 hours ago"}
+             {:href "#"
+              :title "The Micro-Service Architecture"
+              :user "devin"
+              :domain "youtube.com"
+              :created-at "1 day ago"}
+             {:href "#"
+              :title "Spritz (read things fast/comfortably"
+              :user "josh"
+              :domain "sprintzinc.com"
+              :created-at "4 days ago"}])
+
+(def initial-db-value
+  {:users users
+   :links links})
+
+(def db (atom initial-db-value))
+
+(defn get-state []
+  (let [db-val @db]
+    {:users (:users db-val)
+     :links (rseq (:links db-val))}))
+
+(defn new-link [{:keys [title url username]}]
+  {:title title
+   :url url
+   :domain ""
+   :user username
+   :created-at (java.util.Date.)})
+
+(defn add-link [db-val details]
+  (update-in db-val [:links] conj (new-link details)))
+
+(defn submit-link [details]
+  (swap! db add-link details)
+  (get-state))
---
+++ src/cljs/gnar/state/gnar.cljs
@@ -0,0 +1,27 @@
+(ns gnar.state.gnar
+  (:require-macros
+    [tailrecursion.javelin :refer [defc defc= cell=]])
+  (:require
+    [clojure.set           :as cs]
+    [clojure.string        :as s]
+    [tailrecursion.javelin :as j :refer [cell]]
+    [tailrecursion.castra  :as c :refer [mkremote]]))
+
+
+(set! cljs.core/*print-fn* #(.log js/console %))
+
+(defc state {})
+(defc error nil)
+(defc loading [])
+
+(defc= links (:links state))
+
+(def get-state
+  (mkremote 'gnar.api.gnar/get-state state error (cell nil)))
+
+(def submit-link!
+  (mkremote 'gnar.api.gnar/submit-link state error loading) )
+
+(defn init []
+  (get-state)
+  (js/setInterval get-state 1000))
